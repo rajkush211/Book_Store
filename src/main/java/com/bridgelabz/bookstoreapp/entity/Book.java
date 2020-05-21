f8294da26b5043d97568f55961d27ccd97456f05
rajkush211 <rajkush211.rk@gmail.com>
2020-05-21 23:04:04 +0530

    ADDED : Book Entity in entity package

---
+++ src/main/java/com/bridgelabz/bookstoreapp/entity/Book.java
@@ -0,0 +1,70 @@
+package com.bridgelabz.bookstoreapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String author;
+    private String nameOfBook;
+    private String picPath;
+    private int price;
+    @Column(length = 2000)
+    private String description;
+
+    public Book() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public String getPicPath() {
+        return picPath;
+    }
+
+    public void setPicPath(String picPath) {
+        this.picPath = picPath;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
+
