2578d147ea0743951e2eb13d0857901acf2ac64c
rajkush211 <rajkush211.rk@gmail.com>
2020-05-25 03:05:59 +0530

    ADDED : Default Page size to 10 and page no. to 0 which is 1

--- src/main/java/com/bridgelabz/bookstoreapp/controller/UserController.java
+++ src/main/java/com/bridgelabz/bookstoreapp/controller/UserController.java
@@ -5,6 +5,7 @@ import com.bridgelabz.bookstoreapp.service.IBookStoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,22 +21,22 @@ public class UserController {
     private IBookStoreService iBookStoreService;
 
     @GetMapping("/all-book")
-    public ResponseEntity<Page<Book>> getAllBook(Pageable pageable) {
+    public ResponseEntity<Page<Book>> getAllBook(@PageableDefault(page = 0, size = 10) Pageable pageable) {
         return new ResponseEntity(iBookStoreService.getAllBook(pageable), HttpStatus.OK);
     }
 
     @GetMapping("/{author}")
-    public ResponseEntity<Page<Book>> booksByAuthor(@PathVariable String author, Pageable pageable) {
+    public ResponseEntity<Page<Book>> booksByAuthor(@PathVariable String author, @PageableDefault(page = 0, size = 10) Pageable pageable) {
         return new ResponseEntity(iBookStoreService.findByAuthor(author, pageable), HttpStatus.OK);
     }
 
     @GetMapping("/sort-asc/price")
-    public ResponseEntity<Page<Book>> booksInAscendingOrderByPrice(Pageable pageable) {
+    public ResponseEntity<Page<Book>> booksInAscendingOrderByPrice(@PageableDefault(page = 0, size = 10) Pageable pageable) {
         return new ResponseEntity(iBookStoreService.getAllBookByPriceAsc(pageable), HttpStatus.OK);
     }
 
     @GetMapping("/sort-desc/price")
-    public ResponseEntity<Page<Book>> booksInDescendingOrderByPrice(Pageable pageable) {
+    public ResponseEntity<Page<Book>> booksInDescendingOrderByPrice(@PageableDefault(page = 0, size = 10) Pageable pageable) {
         return new ResponseEntity(iBookStoreService.getAllBookByPriceDesc(pageable), HttpStatus.OK);
     }
 }
