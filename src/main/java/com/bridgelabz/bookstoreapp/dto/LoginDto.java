efcbb78a1f1c5a43889b25e7699a78a4bf0c2d22
rajkush211 <rajkush211.rk@gmail.com>
2020-05-25 06:03:20 +0530

    ADDED : LoginDto class

---
+++ src/main/java/com/bridgelabz/bookstoreapp/dto/LoginDto.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.bookstoreapp.dto;
+
+public class LoginDto {
+
+    String emailId;
+    String passWord;
+
+    public LoginDto() {
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
