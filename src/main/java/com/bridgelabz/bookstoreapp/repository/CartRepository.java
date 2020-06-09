95a8b7496f7048747e4ae82f1ed5d6a36de9a2c2
rajkush211 <rajkush211.rk@gmail.com>
2020-06-09 08:10:42 +0530

    REFACTORED : Cart Repository to extend JpaRepository to persist the data

--- src/main/java/com/bridgelabz/bookstoreapp/repository/CartRepository.java
+++ src/main/java/com/bridgelabz/bookstoreapp/repository/CartRepository.java
@@ -1,4 +1,7 @@
 package com.bridgelabz.bookstoreapp.repository;
 
-public interface CartRepository {
+import com.bridgelabz.bookstoreapp.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Integer> {
 }
